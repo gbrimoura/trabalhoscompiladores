c0a1c0eb1be6f97b990acac405620b3271967c7b
gbrimoura <gbrimoura@github.com>
2025-11-11 18:11:19 -0300

    Inicio do trabalho 1

---
+++ JavaCompiler/AnaliseLexica.java
@@ -0,0 +1,79 @@
+import java.io.*;
+
+enum TokenType { NUM, SOMA, MULT, SUB, DIV,  APar, FPar, EOF }
+
+class Token {
+    String lexema;
+    TokenType token;
+    
+    Token(String l, TokenType t) {
+        lexema = l;
+        token = t;
+    }
+}
+
+class AnaliseLexica {
+    BufferedReader arquivo;
+    
+    AnaliseLexica(String a) throws Exception {
+        this.arquivo = new BufferedReader(new FileReader(a));
+    }
+    
+    Token getNextToken() throws Exception {
+        Token token;
+        int eof = -1;
+        char currchar;
+        int currchar1;
+        String currnum;
+        
+        do {
+            currchar1 = arquivo.read();
+            currchar = (char) currchar1;
+        } while (currchar == '\n' || currchar == ' ' || currchar == '\t' || currchar == '\r');
+        
+        if (currchar1 != eof && currchar1 != 10) {
+            if (currchar >= '0' && currchar <= '9') {
+                currnum = "" + currchar;
+                
+                arquivo.mark(1);
+                currchar1 = arquivo.read();
+                currchar = (char) currchar1;
+                
+                while (currchar1 != eof && currchar >= '0' && currchar <= '9') {
+                    currnum = currnum + currchar;
+                    arquivo.mark(1);
+                    currchar1 = arquivo.read();
+                    currchar = (char) currchar1;
+                }
+                
+                if (currchar1 != eof) {
+                    arquivo.reset();
+                }
+                
+                return new Token(currnum, TokenType.NUM);
+            } else {
+                switch (currchar) {
+                    case '(':
+                        return new Token(String.valueOf(currchar), TokenType.APar);
+                    case ')':
+                        return new Token(String.valueOf(currchar), TokenType.FPar);
+                    case '+':
+                        return new Token(String.valueOf(currchar), TokenType.SOMA);
+                    case '*':
+                        return new Token(String.valueOf(currchar), TokenType.MULT);
+		    case '-':
+                        return new Token(String.valueOf(currchar), TokenType.SUB);
+		    case '/':
+                        return new Token(String.valueOf(currchar), TokenType.DIV);
+ 
+ 
+                    default:
+                        throw new Exception("Caractere inválido: " + ((int) currchar));
+                }
+            }
+        }
+        
+        arquivo.close();
+        return new Token("", TokenType.EOF);
+    }
+}
